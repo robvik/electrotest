126ee1b0a568f1fead893fa5619aa20f8f35b574
Jonah <26759317+Jonah-AG@users.noreply.github.com>
2017-10-15 13:41:51 +0200

    Fixade till algoritmen

--- src/libcomponent.c
+++ src/libcomponent.c
@@ -3,7 +3,7 @@
  *  Author: Jonah Ekelund
  *
  */
-
+#include <stdio.h>
 
 /*
  * Function: e_resistance
@@ -17,19 +17,20 @@ int e_resistance(float orig_resistance, float *res_array)
 {
     //E-12 serien deklarerad som en const array
     const float e12Series[37] = {10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68, 82, 100\
-                        , 120, 150, 180, 220, 270, 330, 390, 470, 560, 680, 820, 1000\
-                        , 1200, 1500, 1800, 2200, 2700, 3300, 3900, 4700, 5600, 6800, 8200, 10000};
+        , 120, 150, 180, 220, 270, 330, 390, 470, 560, 680, 820, 1000\
+            , 1200, 1500, 1800, 2200, 2700, 3300, 3900, 4700, 5600, 6800, 8200, 10000};
 
     int count=0;//Return variablen
-    int i;//En counter vairiabel som används internt
-
+    int i=0;//En counter vairiabel som används internt
+    int i_old = 0;
     do{
+        i_old = i;
         //Går uppifrån i e-12 arrayen och hittar första värdet då
         // orig_resistance - e12Series[i] inte är mindre än noll
-        for(i=36; orig_resistance-e12Series[i]<=0 && i>=0;i--);
+        for(i=36; orig_resistance-e12Series[i]<0 && i>=0;i--);
 
-        if(orig_resistance-e12Series[i]>=0)
-        {
+        if(i>=0)
+        {        
             orig_resistance = orig_resistance-e12Series[i];
 
             //Sparar in resistansen i output arrayen
@@ -38,7 +39,11 @@ int e_resistance(float orig_resistance, float *res_array)
             count++;
         }
 
+    }while(i>=0&&count<3);//Loppar medans antallet resistorer är mindre än 3 eller i inte gått ner till 0 
+    
+    orig_resistance = orig_resistance+e12Series[i_old];
+    if(orig_resistance-e12Series[i_old] > e12Series[i_old+1]-orig_resistance) 
+        *(--res_array)=e12Series[i_old+1];
 
-    }while(i>=0&&count<3);//Loppar medans antallet resistorer är mindre än 3 eller i inte gått ner till 0
     return count;
-    }
+}
