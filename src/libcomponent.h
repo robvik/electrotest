b54c396f7ee7fd855e65050ef08588041cc32f84
Jonah <26759317+Jonah-AG@users.noreply.github.com>
2017-10-15 11:58:38 +0200

    Kommenterade

--- src/libcomponent.h
+++ src/libcomponent.h
@@ -2,6 +2,14 @@
 #define __COMPONENT_H__
 
 
+/*
+ * Function: e_resistance
+ * ----------------------------------
+ *  Beräknar vilka resisitoerer i E-12 seriend
+ *  som kan ersätta orig_resistance, max 3 stycken
+ *
+ *  return: (int) antalet resistander i arrayen res_array
+ */
 int e_resistance(float orig_resistance, float *res_array);
 
 
